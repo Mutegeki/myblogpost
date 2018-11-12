f2081d2aa1adca430551086455e6dc73930b71dc
Mutegeki <braynmute@gmail.com>
2018-11-12 18:50:40 +0300

    Connected postgresql

--- blog.py
+++ blog.py
@@ -1,6 +1,18 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://post:post123@localhost/blog'
+db = SQLAlchemy(blog)
+
+class Person(db.Model):
+    id = db.Column(db.integer, primary_key=True)
+    fisrtName = db.Column(db.string(120), unique=False)
+
+    def __init__(self, fisrtName):
+        self.fisrtName = fisrtName
 
 @app.route('/')
 def index():
@@ -19,4 +31,5 @@ def post():
     return render_template('post.html')
 
 if __name__ == '__main__:':
+    db.create_all()
     blog.run(debug=True)
